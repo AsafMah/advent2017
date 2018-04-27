e918efc2c4f0af6fb3f92c2c84403dab4025db89
asaf <mahlev.asaf@gmail.com>
2018-04-27 10:49:51 +0300

    day 5

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,17 @@
-use std::collections::HashSet;
-
 fn main() {
-    let input: &'static str = include_str!("input_day_6");
-    let mut vec: Vec<u32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let mut a = HashSet::new();
-    let mut i = 0;
-    let mut golden_state : Option<Vec<_>> = None;
+    let input : &'static str = include_str!("input_day_5");
+    let mut vec : Vec<i32>  = input.lines().map(|x| x.parse().unwrap()).collect();
+    let mut i : i32 = 0;
+    let mut counter = 0;
     loop {
-        eprintln!("vec = {:?}", (i, &vec));
-        match golden_state {
-            None => {
-                if !a.insert(vec.clone()) {
-                    golden_state = Some(vec.clone());
-                    i = 0;
-                }
-            }
-            Some(ref v) => {
-                if (vec == *v)
-                    {
-                        eprintln!("i = {:?}", i);
-                        break;
-                    }
-            }
-        }
+        let ins = vec[i as usize];
+        vec[i as usize] = if ins >= 3 { vec[i as usize] - 1} else { vec[i as usize] + 1 } ;
+        i += ins;
+        counter += 1;
 
-        i += 1;
-
-        let (max_index, max) = vec.iter().enumerate().fold((0, 0), |(max_ind, max), (ind, &elem)| {
-            if elem > max {
-                (ind, elem)
-            } else {
-                (max_ind, max)
-            }
-        });
-
-        vec[max_index] = 0;
-
-        let mut index = ((max_index + 1) % vec.len()) as usize;
-        let mut current = max;
-
-        loop {
-            vec[index] += 1;
-            current -= 1;
-            if current <= 0 {
-                break;
-            }
-            index = (index + 1) % vec.len();
+        if i < 0 || i >= (vec.len() as i32) {
+            eprintln!("counter = {:?}", counter);
+            break;
         }
     }
 }
