a1ad9b4ef94b705c5449b0127e5d97a673a054a6
asaf <mahlev.asaf@gmail.com>
2018-05-11 15:16:23 +0300

    day 10 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,44 @@
-fn main() {
-    let input: &'static str = include_str!("input_day_9");
-    let mut stack = Vec::new();
-    let mut escape = false;
-    let mut garbage = false;
-    let mut score = 0;
-    let mut count = 0;
 
-    for c in input.chars() {
-        match c {
-            _ if escape => {
-                escape = false;
-            },
-            '!' => {
-                escape = true;
-            },
-            '>' => {
-                stack.pop();
-                garbage = false;
-            },
-            _ if garbage => {
-                count += 1;
-            },
-            '{' => {
-                stack.push('{');
-                score += stack.len();
-            },
-            '}' => {
-                stack.pop();
-            },
-            '<' => {
-                stack.push('<');
-                garbage = true;
-            },
-            _ => {}
+#[derive(Debug)]
+struct List {
+    arr: Vec<u32>,
+    index: usize,
+    skip: usize
+}
+
+impl List {
+    fn new() -> Self {
+        Self {
+            arr: (0..=255).collect(),
+            index: 0,
+            skip: 0
         }
     }
 
-    eprintln!("score = {:?}, count = {:?}", score, count);
+    fn do_move(&mut self, len: usize) {
+        for i in 0..(len / 2) {
+            let start_index = (self.index + i) % self.arr.len();
+            let end_index = (self.index + len - i - 1) % self.arr.len();
+            self.arr.swap(start_index, end_index);
+        }
+        self.index += len + self.skip;
+        self.skip += 1;
+
+    }
+}
+
+
+fn main() {
+    let input: &'static str = include_str!("input_day_10");
+
+    let inputs : Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
+
+    let mut list = List::new();
+
+    for length in inputs {
+        eprintln!("list = {:?}\n", list);
+        list.do_move(length);
+    }
+
+    eprintln!("list = {:?}, mul = {:?}", list, list.arr[0] * list.arr[1]);
 }
