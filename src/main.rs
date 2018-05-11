24cb8a84676df92d69a57fed49c71667bea733ca
asaf <mahlev.asaf@gmail.com>
2018-05-11 15:38:17 +0300

    day 10 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 #[derive(Debug)]
 struct List {
     arr: Vec<u32>,
@@ -29,16 +28,21 @@ impl List {
 
 
 fn main() {
-    let input: &'static str = include_str!("input_day_10");
-
-    let inputs : Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
+    let input: &'static [u8] = include_bytes!("input_day_10");
+    let input : Vec<_> = input.iter().chain(&[17u8, 31, 73, 47, 23]).collect();
 
     let mut list = List::new();
 
-    for length in inputs {
-        eprintln!("list = {:?}\n", list);
-        list.do_move(length);
+    for &&length in input.iter().cycle().take(64 * input.len()) {
+        list.do_move(length as usize);
     }
 
-    eprintln!("list = {:?}, mul = {:?}", list, list.arr[0] * list.arr[1]);
+    let str : String = list.arr
+        .chunks(16)
+        .map(|chunk|
+            chunk.iter().fold(0u8, |acc, &i| acc ^ (i as u8)))
+        .map(|b| format!("{:02X}", b))
+        .collect();
+
+    eprintln!("str = {}", str);
 }
