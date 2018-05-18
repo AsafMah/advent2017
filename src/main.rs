db0c227c97a49ae91cc99ee1f1fda7b5fd0b21a0
asaf <mahlev.asaf@gmail.com>
2018-05-18 09:41:07 +0300

    day 11

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
+use std::str::FromStr;
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+enum Direction {
+    North,
+    South,
+    NorthEast,
+    SouthEast,
+    NorthWest,
+    SouthWest,
+}
+
+impl Direction {
+    fn parse(str: &str) -> Direction {
+        match str {
+            "n" => Direction::North,
+            "s" => Direction::South,
+            "ne" => Direction::NorthEast,
+            "se" => Direction::SouthEast,
+            "nw" => Direction::NorthWest,
+            "sw" => Direction::SouthWest,
+            _ => panic!("np")
+        }
+    }
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        Ok(Self::parse(s))
+    }
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Copy, Clone)]
+struct Grid {
+    x: i32,
+    y: i32,
+}
+
+impl Grid {
+    fn next(&mut self, dir: Direction) {
+        match dir {
+            Direction::North => {self.y += 2;},
+            Direction::South => {self.y -= 2;},
+            Direction::NorthEast => {self.y += 1;self.x += 1;},
+            Direction::SouthEast => {self.y -= 1;self.x += 1;},
+            Direction::NorthWest => {self.y += 1;self.x -= 1;},
+            Direction::SouthWest => {self.y -= 1;self.x -= 1;},
+        }
+    }
+
+    fn smart_step(&mut self) {
+        let dir = if self.y.abs() == self.x.abs() {
+            match (self.x.signum(), self.y.signum()) {
+                (1, 1) => Direction::SouthWest,
+                (1, -1) => Direction::NorthWest,
+                (-1, 1) => Direction::SouthEast,
+                (-1, -1) => Direction::NorthEast,
+                _ => panic!("at the disco")
+            }
+        }
+        else {
+            if self.y > self.x {
+                Direction::South
+            }
+            else {
+                Direction::North
+            }
+        };
+
+        self.next(dir)
+    }
+}
+
 
 fn main() {
     let input: &'static str = include_str!("input_day_11");
