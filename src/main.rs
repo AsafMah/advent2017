d33ecc6f857eab14008a0b256a22ec5292a4f265
asaf <mahlev.asaf@gmail.com>
2018-05-19 15:19:30 +0300

    day 13

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,31 @@
-use std::collections::HashSet;
 use std::collections::HashMap;
 
 fn main() {
-    let input: &'static str = include_str!("input_day_12");
+    let input: &'static str = include_str!("input_day_13");
 
-    let mut data : HashMap<usize, Vec<usize>> = input.lines().enumerate()
-        .map(|(i, l)|
-                 (i, l.split(" <-> ")
-                .nth(1).unwrap()
-                .split(", ")
-                .map(|d| d.parse().unwrap())
-                .collect()))
+    let data: HashMap<usize, usize> = input.lines()
+        .map(|l| {
+            let mut a = l.split(": ");
+            (a.next().unwrap().parse().unwrap(), a.next().unwrap().parse().unwrap())
+        })
         .collect();
 
+    for i in 0.. {
+        let sum = data.iter().fold(0, |acc, (&depth, &range)| {
+            let pos = (depth + i) % ((range - 1) * 2);
+            acc + if pos == 0  {
+                //(depth * range)
+                1
+            } else {
+                0
+            }
+        });
 
-    let mut results_vec = Vec::new();
-
-    loop {
-        if data.is_empty() {
+        if sum == 0 {
+            eprintln!("data = {:?}", i);
             break;
         }
-
-        let mut results  = HashSet::new();
-
-        results.insert(*data.keys().min().unwrap());
-
-        for _ in 0..data.len() {
-            for (&i, cons) in data.iter() {
-                if results.contains(&i) {
-                    continue;
-                }
-                for con in cons {
-                    if results.contains(con) {
-                        results.insert(i);
-                        break;
-                    }
-                }
-            }
-        }
-
-        eprintln!("results = {:?}", results);
-        for result in results.iter() {
-            data.remove(&result);
-        }
-        results_vec.push(results);
     }
 
-
-
-    eprintln!("results = {:?}", results_vec.len());
 
 }
