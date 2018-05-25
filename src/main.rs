97f78eed93a27a66a3a9be4442e4be0ef806d07a
asaf <mahlev.asaf@gmail.com>
2018-05-25 14:17:39 +0300

    day 14 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,82 @@
-use std::collections::HashMap;
+#[derive(Debug)]
+struct List {
+    arr: Vec<u32>,
+    index: usize,
+    skip: usize,
+}
+
+impl List {
+    fn new() -> Self {
+        Self {
+            arr: (0..=255).collect(),
+            index: 0,
+            skip: 0,
+        }
+    }
+
+    fn do_move(&mut self, len: usize) {
+        for i in 0..(len / 2) {
+            let start_index = (self.index + i) % self.arr.len();
+            let end_index = (self.index + len - i - 1) % self.arr.len();
+            self.arr.swap(start_index, end_index);
+        }
+        self.index += len + self.skip;
+        self.skip += 1;
+    }
+}
+
+fn u8_to_bit_array(n: u8) -> [bool; 8] {
+    let mut res = [false; 8];
+    let mut current = n;
+    for i in (0..res.len()).rev() {
+        res[i] = (current & 1) == 1;
+        current >>= 1;
+    }
+    res
+}
+
+fn walk(vec: &mut Vec<Vec<bool>>, index: (usize, usize)) {
+    if vec[index.0][index.1] == false {
+        return;
+    }
+    vec[index.0][index.1] = false;
+    if index.0 + 1 != vec.len() {
+        let new_index = (index.0 + 1, index.1);
+        walk(vec, new_index);
+    }
+    if index.0  != 0 {
+        walk(vec, (index.0 - 1, index.1));
+    }
+    if index.1 + 1 != vec.len() {
+        walk(vec, (index.0, index.1 + 1));
+    }
+    if index.1  != 0 {
+        walk(vec, (index.0, index.1 - 1));
+    }
+}
 
 fn main() {
-    let input: &'static str = include_str!("input_day_13");
+    let input: &'static str = "amgozmfv";
+    let mut vec = (0..128).map(|i| {
+        let line = format!("{}-{}", input, i);
+        knot_hash(line.as_bytes())
+    })
+        .map(|vec| vec.iter().flat_map(|&b| u8_to_bit_array(b).to_vec()).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
-    let data: HashMap<usize, usize> = input.lines()
-        .map(|l| {
-            let mut a = l.split(": ");
-            (a.next().unwrap().parse().unwrap(), a.next().unwrap().parse().unwrap())
-        })
-        .collect();
+    let mut counter = 0;
 
-    for i in 0.. {
-        let sum = data.iter().fold(0, |acc, (&depth, &range)| {
-            let pos = (depth + i) % ((range - 1) * 2);
-            acc + if pos == 0  {
-                //(depth * range)
-                1
-            } else {
-                0
+    for i in 0..vec.len() {
+        for j in 0..vec[i].len() {
+            if vec[i][j] {
+                counter += 1;
+                walk(&mut vec, (i, j));
             }
-        });
-
-        if sum == 0 {
-            eprintln!("data = {:?}", i);
-            break;
         }
     }
 
 
+    eprintln!("counter = {:?}", counter);
 }
 
 fn knot_hash(input: &[u8]) -> Vec<u8> {
