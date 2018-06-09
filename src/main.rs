a4dbdde6a75fd25dd58afe8c3e7f75965e5c1e54
asaf <mahlev.asaf@gmail.com>
2018-06-09 10:06:23 +0300

    day 17 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,17 @@
-#[macro_use]
-extern crate nom;
+use std::error::Error;
 
-use std::char;
-use std::str::{FromStr, from_utf8};
-use nom::{digit, types::CompleteByteSlice};
+fn main() -> Result<(), Box<Error>>  {
+    let input: usize = include_str!("input_day_17.txt").parse()?;
+    let mut buf = vec![0];
+    let mut current = 0;
 
-named!(letter_to_num<CompleteByteSlice, usize>,
-    map!(take!(1), |b| (b[0] as usize) - ('a' as usize) )
-);
-
-named!(number<CompleteByteSlice, usize>,
-    map_res!(map_res!(map!(digit, |b| b.0), from_utf8), FromStr::from_str)
-);
-
-named!(parse_move<CompleteByteSlice, Move>,
-alt!(
-    do_parse!(
-    char!('s') >>
-    count: number >>
-    (Move::Spin(count))
-    ) |
-    do_parse!(
-    char!('x') >>
-    first: number >>
-    char!('/') >>
-    second: number >>
-    (Move::Exchange(first, second))
-    ) |
-    do_parse!(
-    char!('p') >>
-    first: letter_to_num >>
-    char!('/') >>
-    second: letter_to_num >>
-    (Move::Partner(first, second))
-    )
-    )
-);
-
-
-named!(parse_moves<CompleteByteSlice, Vec<Move>>,
-    separated_list!(char!(',') , parse_move)
-);
-
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-enum Move {
-    Spin(usize),
-    Exchange(usize, usize),
-    Partner(usize, usize),
-}
-
-fn main()  {
-    let input: &'static str = include_str!("input_day_16.txt");
-    let mut state: Vec<_> = (0..16).collect();
-    let mut states = vec![state.clone()];
-    eprintln!("state = {:?}", state);
-    let data2 = parse_moves(CompleteByteSlice(input.as_bytes()));
-    let data2 = data2.unwrap().1;
-
-    for _ in 0..1_000_000_000 {
-        for dance_move in &data2 {
-            match dance_move {
-                Move::Spin(count) => state.rotate_right(*count),
-                Move::Exchange(i1, i2) => state.swap(*i1, *i2),
-                Move::Partner(p1, p2) => {
-                    let pos1 = state.iter().position(|c| c == p1).unwrap();
-                    let pos2 = state.iter().position(|c| c == p2).unwrap();
-                    state.swap(pos1, pos2)
-                }
-            };
-        }
-
-        if states[0] == state {
-            break;
-        }
-
-        states.push(state.clone());
-
+    for i in 1..2018 {
+        let new_position = (current + input) % buf.len();
+        buf.insert(new_position, i);
+        current = new_position + 1;
     }
 
-    let states_strings = states.iter().map(|s| s.iter().map(|c| char::from_u32((c + 97) as u32).unwrap()).collect::<String>()).collect::<Vec<_>>();
+    eprintln!("buf = {:?}", buf[current]);
 
-    eprintln!("data = {:?}", states_strings);
-    eprintln!("1_000_000_000 % states.len() = {:?}", states_strings[1_000_000_000 % states.len()]);
+    Ok(())
 }
