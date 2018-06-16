d77323acea56c12c691ca01e67b668a173297892
asaf <mahlev.asaf@gmail.com>
2018-06-16 17:09:07 +0300

    day 17 part 2

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use std::error::Error;
 fn main() -> Result<(), Box<Error>>  {
     let input: usize = include_str!("input_day_17.txt").parse()?;
     let mut buf = vec![0];
+    buf.reserve(50_000_000);
+    let mut position = 0;
     let mut current = 0;
 
-    for i in 1..2018 {
-        let new_position = (current + input) % buf.len();
-        buf.insert(new_position, i);
-        current = new_position + 1;
+    for i in 1..50_000_000 {
+        position = (position + input) % i + 1;
+        if  position == 1 {
+            current = i;
+        }
     }
 
-    eprintln!("buf = {:?}", buf[current]);
+    eprintln!("current = {:?}", current);
 
     Ok(())
 }
