f1d6ed15ce7a534c6374147106684997ddca22a2
asaf <mahlev.asaf@gmail.com>
2018-07-13 10:00:49 +0300

    not working but I want it commited

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,90 @@
 extern crate failure;
 extern crate itertools;
-#[macro_use]
-extern crate nom;
-extern crate multimap;
+extern crate ndarray;
 
 use failure::Error;
-use nom::types::CompleteStr;
-use nom::line_ending;
-use multimap::MultiMap;
 use std::collections::HashMap;
+use ndarray::Array2;
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Debug, Default, Hash)]
-struct Vec3 {
-    x: i64,
-    y: i64,
-    z: i64,
-}
+type Slot = u8;
+type GridHash = Vec<Slot>;
 
-impl Vec3 {
-    fn texicab_distance(&self) -> i64 {
-        self.x.abs() + self.y.abs() + self.z.abs()
+#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Debug, Hash)]
+struct Grid(Vec<Vec<Slot>>);
+
+impl Grid {
+    fn hash(&self) -> GridHash {
+        let mut sum_vec = Vec::new();
+
+        for v in self.0.iter() {
+            sum_vec.push(v.iter().sum());
+        }
+
+        let mut sum_top_left_to_bottom_right = 0;
+        let mut sum_top_right_to_bottom_left = 0;
+
+        for i in 0..self.0.len() {
+            let mut sum = 0;
+            for j in 0..self.0[i].len() {
+                sum += self.0[j][i];
+            }
+            sum_top_left_to_bottom_right += self.0[i][i];
+            sum_top_right_to_bottom_left += self.0[self.0.len() - 1 - i][self.0.len() - 1 - i];
+            sum_vec.push(sum);
+        }
+        sum_vec.push(sum_top_left_to_bottom_right);
+        sum_vec.push(sum_top_right_to_bottom_left);
+
+        sum_vec.sort();
+        sum_vec
+    }
+
+    fn enrich(&mut self, pattern_map: &HashMap<GridHash, Grid>) {
+        let chunk_size = if self.0.len() % 2 == 0 { 2 } else { 3 };
+
+        for i in (0..self.0.len()).step_by(chunk_size) {
+            for j in (0..self.0[i].len()).step_by(chunk_size) {
+                let grid = Grid(self.0[i..(i+chunk_size)].iter().map(|v| v[j..(j+chunk_size)].into()).collect());
+                let new_grid = &pattern_map[&grid.hash()];
+                eprintln!("new_grid = {:?}", new_grid);
+            }
+        }
     }
 }
 
-
-#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Debug, Default, Hash)]
-struct Particle {
-    pos: Vec3,
-    vel: Vec3,
-    acc: Vec3
-}
-
-impl Particle {
-    fn distance(&self) -> i64 {
-        self.pos.texicab_distance()
-    }
-
-    fn do_step(&mut self) {
-        self.vel.x += self.acc.x;
-        self.vel.y += self.acc.y;
-        self.vel.z += self.acc.z;
-        self.pos.x += self.vel.x;
-        self.pos.y += self.vel.y;
-        self.pos.z += self.vel.z;
+fn map_to_byte(c: char) -> Slot {
+    match c {
+        '#' => 1,
+        '.' => 0,
+        _ => {
+            eprintln!("c = {:?}", c);
+            unreachable!()
+        }
     }
 }
-
-
-fn is_number(chr: char) -> bool {
-    chr >= '0' && chr <= '9' || chr == '-'
-}
-
-named!(number<CompleteStr, i64>,
-    flat_map!(take_while!(is_number), parse_to!(i64))
-);
-
-named!(vec3<CompleteStr, Vec3>,
-    do_parse!(
-        tag!("<") >>
-        x: number >>
-        tag!(",") >>
-        y: number >>
-        tag!(",") >>
-        z: number >>
-        tag!(">") >>
-        (Vec3 { x, y, z})
-    )
-);
-
-
-named!(particle<CompleteStr, Particle>,
-    do_parse!(
-        tag!("p=") >>
-        pos: vec3 >>
-        tag!(", v=") >>
-        vel: vec3 >>
-        tag!(", a=") >>
-        acc: vec3 >>
-        (Particle { pos, vel, acc})
-    )
-);
-
-named!(particles<CompleteStr, Vec<Particle>>,
-    separated_list!(line_ending , particle)
-);
-
 
 fn main() -> Result<(), Error> {
-    let input : &'static str = include_str!("input_day_20");
-    let particles : Vec<Particle> = particles(CompleteStr(input)).unwrap().1;
-    let mut particles : HashMap<usize, Particle> = particles.into_iter().enumerate().collect();
+    let input: &'static str = include_str!("input_day_21");
 
-    for _ in 0..10000 {
-        let mut multimap = MultiMap::new();
+    let mut initial_grid = Grid(
+    vec![
+        vec![0, 1, 0],
+        vec![0, 0, 1],
+        vec![1, 1, 1],
+    ]);
 
-        for (i, particle) in particles.iter_mut() {
-            particle.do_step();
-            multimap.insert(particle.pos, *i);
-        }
 
-        for (_, values) in multimap.iter_all() {
-            if values.len() == 1 {
-                continue;
-            }
+    let mut pattern_map: HashMap<_, _> =
+        input.lines().map(|l| {
+            let mut parts = l.split(" => ");
+            let key = Grid(parts.next().unwrap().split("/").map(|l| l.chars().map(map_to_byte).collect()).collect());
+            let val = Grid(parts.next().unwrap().split("/").map(|l| l.chars().map(map_to_byte).collect()).collect());
 
-            for value in values {
-                particles.remove(value);
-            }
-        }
+            (key.hash(), val)
+        }).collect();
 
-    }
+    initial_grid.enrich(&pattern_map);
 
-    eprintln!("particles.len() = {:?}", particles.len());
+    eprintln!("g = {:?}", pattern_map);
     Ok(())
 }
