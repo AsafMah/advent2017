1d387bc093d7e3c26c6de30555ec4842ad981610
asaf <mahlev.asaf@gmail.com>
2018-08-03 16:15:03 +0300

    day 22 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,54 @@ extern crate failure;
 extern crate itertools;
 extern crate ndarray;
 
-use failure::Error;
-use ndarray::Array2;
-use ndarray::prelude::arr2;
-use std::collections::HashMap;
 use itertools::Itertools;
-use ndarray::Axis;
-use ndarray::stack;
+use failure::Error;
 
-type Slot = u8;
-type PatternMap = HashMap<Array2<Slot>, Grid>;
-
-
-fn find_in_map_reverse_transpose<'a>(map: &'a PatternMap, arr: &Array2<Slot>) -> Result<&'a Grid, Array2<Slot>> {
-    let mut arr = arr.clone().reversed_axes();
-
-    map.get(&arr).or_else(|| {
-        arr.invert_axis(Axis(1));
-        map.get(&arr)
-    }).ok_or_else(|| arr)
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
 }
 
-fn find_in_map<'a>(map: &'a PatternMap, arr: &Array2<Slot>) -> &'a Grid {
-    find_in_map_reverse_transpose(&map, arr)
-        .or_else(|arr| find_in_map_reverse_transpose(&map, &arr))
-        .or_else(|arr| find_in_map_reverse_transpose(&map, &arr))
-        .or_else(|arr| find_in_map_reverse_transpose(&map, &arr))
-        .unwrap()
-}
+impl Direction {
+    fn right(self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
 
-
-#[derive(Clone, Debug, Hash)]
-struct Grid(Array2<Slot>);
-
-impl Grid {
-    fn enrich(&self, map: &PatternMap) -> Grid {
-        let len = if self.0.len_of(Axis(0)) % 2 == 0 { 2 } else { 3 };
-        let v = self.0
-            .exact_chunks((len, len))
-            .into_iter()
-            .map(|c| find_in_map(map, &c.to_owned()).0.view())
-            .collect_vec();
-
-        let new_arrays = v.chunks(self.0.len_of(Axis(0)) / len)
-            .map(|c| stack(Axis(1), c).unwrap())
-            .collect_vec();
-
-        let views = new_arrays.iter().map(|v| v.view()).collect_vec();
-
-
-        Grid(stack(Axis(0), &views).unwrap())
+    fn left(self) -> Direction {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Left => Direction::Down,
+            Direction::Down => Direction::Right,
+            Direction::Right => Direction::Up,
+        }
     }
 }
 
-fn map_to_byte(c: char) -> Slot {
+#[derive(Debug)]
+struct Cursor {
+    x: usize,
+    y: usize,
+    direction: Direction
+}
+
+fn advance(grid: &[Vec<u8>], cursor: &mut Cursor) {
+    match cursor.direction {
+        Direction::Up => cursor.x -= 1,
+        Direction::Right => cursor.y += 1,
+        Direction::Down => cursor.x += 1,
+        Direction::Left => cursor.y -= 1,
+    }
+}
+
+fn map_to_byte(c: char) -> u8 {
     match c {
         '#' => 1,
         '.' => 0,
@@ -66,34 +60,67 @@ fn map_to_byte(c: char) -> Slot {
     }
 }
 
-fn main() -> Result<(), Error> {
-    let input: &'static str = include_str!("input_day_21");
-
-    let initial_grid = Grid(
-        arr2(&[
-            [0, 1, 0],
-            [0, 0, 1],
-            [1, 1, 1],
-        ]));
-
-
-    let pattern_map: PatternMap =
-        input.lines().map(|l| {
-            let mut parts = l.split(" => ");
-            let key: Vec<_> = parts.next().unwrap().split("/").flat_map(|l| l.chars().map(map_to_byte)).collect();
-            let len = if key.len() == 4 { 2 } else { 3 };
-            let key = Array2::from_shape_vec((len, len), key).unwrap();
-
-            let val = parts.next().unwrap().split("/").flat_map(|l| l.chars().map(map_to_byte)).collect();
-            let val = Grid(Array2::from_shape_vec((len + 1, len + 1), val).unwrap());
-            (key, val)
-        }).collect();
-
-    let mut grid = initial_grid;
-    for _ in 0..18 {
-         grid = grid.enrich(&pattern_map);
-        eprintln!("count = {:?}", grid.0.iter().fold(0u64, |acc, &i| acc + (i as u64)));
+fn print_grid(grid: &[Vec<u8>], cursor: &Cursor) {
+    print!("    ");
+    for i in 0..grid.len() {
+        print!("{:2} ", i);
     }
 
+    println!();
+
+    for i in 0..grid.len() {
+        print!("{:02}  ", i);
+        for j in 0..grid.len() {
+            if cursor.x == i && cursor.y == j {
+                print!("[{}]", grid[i][j]);
+            } else {
+                print!(" {} ", grid[i][j]);
+            }
+        }
+        println!();
+    }
+
+    println!();
+
+}
+
+fn main() -> Result<(), Error> {
+    let input: &'static str = include_str!("input_day_22");
+    let vec = input.lines().map(|l| l.chars().map(map_to_byte).collect_vec() ).collect_vec();
+    let mut grid = vec![vec!(0u8;10000);10000];
+    let grid_offset = grid.len() / 2;
+    for i in 0..vec.len(){
+        for j in 0..vec[0].len() {
+            grid[grid_offset + i][j + grid_offset] = vec[i][j];
+        }
+    }
+
+    let mut virus = Cursor {
+        x: grid_offset + vec[0].len() / 2,
+        y: grid_offset + vec.len() / 2,
+        direction: Direction::Up
+    };
+
+    let mut infected_counter = 0;
+
+    for _ in 0..10000 {
+        match grid[virus.x][virus.y] {
+            1 => {
+                grid[virus.x][virus.y] = 0;
+                virus.direction = virus.direction.right();
+            }
+            0 => {
+                grid[virus.x][virus.y] = 1;
+                infected_counter += 1;
+                virus.direction = virus.direction.left();
+                eprintln!("virus = {:?}", virus);
+            }
+            _ => unreachable!()
+        }
+
+        advance(&grid, &mut virus);
+        eprintln!("virus = {:?}", virus);
+    }
+    eprintln!("infected_counter = {:?}", infected_counter);
     Ok(())
 }
