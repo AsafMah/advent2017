ee096ea74e5613536e08c039148bb0c678cf98ac
asaf <mahlev.asaf@gmail.com>
2018-08-03 17:53:46 +0300

    day 24 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
 extern crate failure;
+extern crate itertools;
 
 use failure::Error;
+use itertools::Itertools;
+
+#[derive(Copy, Clone, Debug)]
+struct Outlet(u32, u32);
+
+impl Outlet {
+    fn connects(self, i: u32) -> Option<(u32, u32)>{
+        if self.0 == i {
+            Some((self.0, self.1))
+        }
+        else if self.1 == i {
+            Some((self.1, self.0))
+        }
+        else {
+            None
+        }
+    }
+}
+
+fn strongest_bridge(current: u32, v: &Vec<Outlet>) -> u32{
+    v.iter().enumerate().filter_map(|(i, o)| o.connects(current).map(|s| (i, s.0, s.1)))
+    .map(|(i, new_current, next)| {
+        let mut clone = v.clone();
+        clone.remove(i);
+        current + new_current + strongest_bridge(next, &clone)
+    }).max().unwrap_or(current)
+}
 
 fn main() -> Result<(), Box<Error>> {
-    //read input_day_23 to understand why i'm doing this
-    let mut non_primes = 0;
-    let mut current = 109300;
-    loop {
-        if current > 126300 {
-            break;
-        }
+    let input: &'static str = include_str!("input_day_24");
 
-        let sqrt = (current as f64).sqrt() as i32 + 1;
-        let mut prime = true;
-        'outer: for i in 2..sqrt {
-            for j in i..current {
-                if i * j == current {
-                    non_primes += 1;
-                    prime = false;
-                    break 'outer;
-                }
-            }
+    let vec = input.lines().map(|l| {
+        let mut split = l.split("/");
+        Outlet(split.next().unwrap().parse().unwrap(), split.next().unwrap().parse().unwrap())
+    }).collect_vec();
 
-        }
-
-        if prime {
-            eprintln!("prime = {:?}", current);
-        }
-
-        current += 17;
-
-    }
-    eprintln!("current, non_primes = {:?}", (current, non_primes));
+    ;
+    eprintln!("vec = {:?}", strongest_bridge(0, &vec));
 
     Ok(())
 }
